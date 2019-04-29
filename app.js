8a87d75c7238f9b34849bf1906759965085ccb45
Blaise Demaree <bdemaree@Michaels-MacBook-Pro.local>
2019-04-29 13:46:32 -0700

    Added vanilla bootstrap

--- app.js
+++ app.js
@@ -3,15 +3,21 @@ const app = express()
 
 var exphbs = require('express-handlebars');
 
-
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 
 app.set('view engine', 'handlebars');
 
+var events = [
+  { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+  { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+  { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
+]
 
-app.get('/', (req, res) => {
-  res.render('home', { msg: 'Handlebars are Cool!' });
-})
+
+
+app.get('/events', (req, res) => {
+  res.render('events-index', { events: events });
+});
 
 
 const port = process.env.PORT || 3000;
